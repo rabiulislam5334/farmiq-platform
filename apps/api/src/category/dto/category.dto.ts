a803fa17a7ded2ad24ee1f63101bbf8bef854d50
rabiulislam5334 <rabiulislam5334@gmail.com>
2026-06-20 20:10:19 +0600

    feat: farm module crud - tested and working

---
+++ apps/api/src/category/dto/category.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsOptional } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  icon?: string;
+
+  @IsString()
+  slug: string;
+}
